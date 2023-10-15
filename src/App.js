dd81bf0f4a0705b212fc9e9293f42b7e8c5253f0
janieshbavan <118841187+janieshbavan@users.noreply.github.com>
2023-10-15 12:27:00 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from './components/Container';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
+
